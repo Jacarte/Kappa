1cb5ba84231b29b0619806da15f30ea0360e0ca8
Javier Cabrera <xppcoder@gmail.com>
2023-09-11 16:00:11 +0200

    WIP

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 echo "Compiling"
 
 # export NOWIDOW=True
-export ADDCONTRIB=True
+# export ADDCONTRIB=True
 pdflatex -draftmode -interaction=nonstopmode -shell-escape Kappa.tex || true
 bibtex Kappa || true
 pdflatex -interaction=nonstopmode -shell-escape Kappa.tex || true
