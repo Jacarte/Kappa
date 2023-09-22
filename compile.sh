b97e2040455aecd1b0af3ff4a234f22e38abe2b9
Javier Cabrera <xppcoder@gmail.com>
2023-09-22 14:59:11 +0200

    WIP

--- compile.sh
+++ compile.sh
@@ -4,4 +4,5 @@ echo "Compiling"
 # export ADDCONTRIB=True
 pdflatex -draftmode -interaction=nonstopmode -shell-escape Kappa.tex || true
 bibtex Kappa || true
+bibtex sota_wasm || true
 pdflatex -interaction=nonstopmode -shell-escape -synctex=1 Kappa.tex || true
