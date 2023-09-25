b29b980c21653b5df068b1381d2bc46294279bfd
Javier Cabrera <xppcoder@gmail.com>
2023-09-25 12:45:16 +0200

    Swaping managed and unmanaged concepts

--- compile.sh
+++ compile.sh
@@ -4,5 +4,4 @@ echo "Compiling"
 # export ADDCONTRIB=True
 pdflatex -draftmode -interaction=nonstopmode -shell-escape Kappa.tex || true
 bibtex Kappa || true
-bibtex sota_wasm || true
 pdflatex -interaction=nonstopmode -shell-escape -synctex=1 Kappa.tex || true
