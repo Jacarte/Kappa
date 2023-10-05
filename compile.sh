46bdfec7e70d208f829bd67b91e225f92bcd80ba
Javier Cabrera <xppcoder@gmail.com>
2023-10-05 11:54:02 +0200

    TYPOS

--- compile.sh
+++ compile.sh
@@ -2,6 +2,21 @@ echo "Compiling"
 
 # export NOWIDOW=True
 # export ADDCONTRIB=True
-pdflatex -draftmode -interaction=nonstopmode -shell-escape Kappa.tex || true
-bibtex Kappa || true
-pdflatex -interaction=nonstopmode -shell-escape -synctex=1 Kappa.tex || true
+ARG=$1
+
+CALL :CASE_%ARG%
+
+:CASE_draft
+pdflatex -interaction=nonstopmode -shell-escape -synctex=1  Kappa.tex 
+echo "Draft generated"
+exit
+
+:CASE_bib
+bibtex Kappa 
+echo "Bib generated"
+exit
+
+:CASE_final
+pdflatex -interaction=nonstopmode -shell-escape -synctex=1 Kappa.tex 
+echo "Final generated"
+exit
